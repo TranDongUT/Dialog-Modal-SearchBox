51ad1094a55b705fbe3c29ebafb4db49420fa997
TranDongUT <84276205+TranDongUT@users.noreply.github.com>
2021-12-20 18:13:15 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const btnSearch = document.querySelector('.search-btn');
 const boxSearch = document.querySelector('.search-box');
+const input = document.querySelector('.search-box input');
 console.log(boxSearch);
 btnSearch.addEventListener('click',()=>{
     boxSearch.classList.toggle('open');
+    input.focus();
 })
